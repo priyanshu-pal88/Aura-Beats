54e7785b1aa53acf13671085f98a00785cc164cb
priyanshu-pal88 <aachipal88@gmail.com>
2025-08-31 23:49:48 +0530

    new commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,5 @@ const app  = require('./src/app')
 const connectToDb = require('./src/db/db')
 
 connectToDb()
-app.listen(3000,()=>{
-    console.log("Server has started")
-})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
